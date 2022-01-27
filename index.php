2b7c5aece6d0b7ef62db8fee1b5a75a0a10d68e8
Diego M <diegomarques66@outlook.com>
2022-01-27 18:06:01 -0300

    att1.0

--- index.php
+++ index.php
@@ -4,14 +4,114 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aula Heroku</title>
+    <link href="css/cssheroku.css" rel="stylesheet">
+    <title>Mega Animes HD</title>
 </head>
+
 <body>
-    <h1>Aula Heroku</h1>
-    <h1>Teste 1</h1>
+    <section id="page">
+        <header>
+            <h1>Mega Animes HD</h1>
+            <div id="divBusca">
+                <input type="text" id="busca" placeholder="Buscar..." />
+                <img id="img1" src="img/icones/search.png" id="btnBusca" alt="Buscar" />
+            </div>
+        </header>
+        <nav>
+            <ul id="navopcoes">
+                <br>
+                <li id="lista">
+                    <img src="img/icones/perfil-g.png" class="icones">
+                    <a href="/">Minha conta</a>
+                </li>
+                <br>
+                <li id="lista">
+                    <img src="img/icones/categoria-g.png" class="icones">
+                    <a href="/">Categorias</a>
+                </li>
+                <br>
+                <li id="lista">
+                    <img src="img/icones/novo-g.png" class="icones">
+                    <a href="/">Lançamentos</a>
+                </li>
+                <br>
+                <li id="lista">
+                    <img src="img/icones/caixa-g.png" class="icones">
+                    <a href="/">Todos os animes</a>
+                </li>
+                <br>
+                <li id="lista">
+                    <img src="img/icones/sair-g.png" class="icones">
+                    <a href="login.html">Sair</a>
+                </li>
+            </ul>
+            <li>
+                <h2 id="emAlta">Em alta</h2>
+            </li>
+            <div class="navimgs">
+                <img src="img/Demon slayer.jpg" id="img2">
+            </div>
+            <div class="navimgs">
+                <img src="img/230px-Shingeki_no_Kyojin_1ª_temporada.jpg" id="img2">
+            </div>
+            <div class="navimgs">
+                <img src="img/jujutsu_capa.png" id="img2">
+            </div>
+        </nav>
+        <main>
+            <div class="logo">
+                <img src="img/logo_transparent.png" id="logo">
+            </div>
+            </div class="cima">
+            <h2 class="epsnovos">Episódios recentes</h2>
+            <div class="img3">
+                <img src="img/icones/seta-esquerda-g.png" id="setae">
+            </div>
+            <div class="img3">
+                <img src="img/Tokyo Revengers.jpg" id="img3">
+            </div>
+            <div class="img3">
+                <img src="img/to your eternity.jpg" id="img3">
+            </div>
+            <div class="img3">
+                <img src="img/icones/seta-direita-g.png" id="setad">
+            </div>
+            <h2 class="fim">Você chegou ao fim</h2>
+            </div>
+            <div class="baixo">
+                <h2 class="maisassistidos">Mais Assistidos</h2>
+                <div class="img4">
+                    <img src="img/icones/seta-esquerda-g.png" id="setae2">
+                </div>
+                <div class="img4">
+                    <img src="img/Naruto_Shippuden_Capa.jpg" id="img4">
+                </div>
+                <div class="img4">
+                    <img src="img/full metal.jpg" id="img4">
+                </div>
+                <div class="img4">
+                    <img src="img/Hunter_x_Hunter_2011_Anime.jpg" id="img5">
+                </div>
+                <div class="img4">
+                    <img src="img/jojo.jpg" id="img6">
+                </div>
+                <div class="img4">
+                    <img src="img/one piece.jpg" id="img7">
+                </div>
+                <div class="img4">
+                    <img src="img/icones/seta-direita-g.png" id="setad2">
+                </div>
+                <div class="img4">
+                    <button id="verMais" autofocus>Ver mais</button>
+                </div>
+            </div>
+        </main>
+    </section>
 </body>
+
 </html>
